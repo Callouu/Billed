0c36310a6a0f7c627c16ac04ef524ab1fc0f27f5
Alexis Bouvry <bouvryalexis@gmail.com>
2025-03-25 21:10:40 +0100

    Add test to NewBill.js

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,17 +2,59 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import {screen, waitFor } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import { ROUTES_PATH} from "../constants/routes.js";
+import router from "../app/Router.js";
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then The icon in vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+            window.localStorage.setItem('user', JSON.stringify({
+              type: 'Employee'
+            }))
+            const root = document.createElement("div")
+            root.setAttribute("id", "root")
+            document.body.append(root)
+            router()
+            window.onNavigate(ROUTES_PATH.NewBill)
+            await waitFor(() => screen.getByTestId('icon-mail'))
+            const windowIcon = screen.getByTestId('icon-mail')
+            const iconActivate = windowIcon.classList.contains("active-icon")
+            expect(iconActivate).toBeTruthy();
     })
   })
-})
+
+  describe("When I am on NewBill Page, there are a form", () => {
+		test("Then all the form input should be render correctly", () => {
+			document.body.innerHTML = NewBillUI();
+
+			const formNewBill = screen.getByTestId("form-new-bill");
+			const type = screen.getAllByTestId("expense-type");
+			const name = screen.getAllByTestId("expense-name");
+			const date = screen.getAllByTestId("datepicker");
+			const amount = screen.getAllByTestId("amount");
+			const vat = screen.getAllByTestId("vat");
+			const pct = screen.getAllByTestId("pct");
+			const commentary = screen.getAllByTestId("commentary");
+			const file = screen.getAllByTestId("file");
+			const submitBtn = document.querySelector("#btn-send-bill");
+
+			expect(formNewBill).toBeTruthy();
+			expect(type).toBeTruthy();
+			expect(name).toBeTruthy();
+			expect(date).toBeTruthy();
+			expect(amount).toBeTruthy();
+			expect(vat).toBeTruthy();
+			expect(pct).toBeTruthy();
+			expect(commentary).toBeTruthy();
+			expect(file).toBeTruthy();
+			expect(submitBtn).toBeTruthy();
+			expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy();
+		});
+	});
+});
