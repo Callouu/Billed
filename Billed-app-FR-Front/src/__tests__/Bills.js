a976d91e2f59c4b13cc846b7d6e3d947acaf95c4
Alexis Bouvry <bouvryalexis@gmail.com>
2025-03-28 00:45:10 +0100

    Add more tests to Bills.js

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,8 +2,9 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import {screen, waitFor, fireEvent} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
+import Bills from "../containers/Bills.js";
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
@@ -58,4 +59,82 @@ describe("Given I am connected as an employee", () => {
       expect(iconEye.length).toBe(4);
     });
   })
+  describe("When I click on eye icon", () => {
+    test("Then It should open the correct bill image", () => {
+      document.body.innerHTML = BillsUI({ data: [] })
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+
+      const modal = document.createElement("div");
+      modal.setAttribute("id", "modaleFile");
+      modal.innerHTML = `<div class="modal-body"></div>`;
+      document.body.append(modal);
+
+      const onNavigate = jest.fn();
+      const bills = new Bills({
+        document,
+        onNavigate,
+        store: null,
+        localStorage: window.localStorage,
+      });
+
+      // Create a mock icon element
+      const icon = document.createElement("div");
+      icon.setAttribute("data-testid", "icon-eye");
+      icon.setAttribute("data-bill-url", "https://example.com/bill.jpg");
+      document.body.append(icon);
+
+      $.fn.modal = jest.fn();
+    
+      // Simulate the click event
+      bills.handleClickIconEye(icon);
+      const modalBody = document.querySelector("#modaleFile .modal-body");
+      expect(modalBody.innerHTML).toContain(
+        `<div style=\"text-align: center;\" class=\"bill-proof-container\"><img width=\"0\" src=\"https://example.com/bill.jpg\" alt=\"Bill\"></div>`
+      );
+      expect($.fn.modal).toHaveBeenCalledWith("show");
+    })
+  })
 })
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on Bills Page and I click on New Bill", () => {
+    test ("Then I should be redirected to NewBill page", () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+
+      document.body.innerHTML = BillsUI({ data: [] });
+
+      const onNavigate = jest.fn((pathname) => {
+        document.body.innerHTML = pathname;
+      });
+      
+      const bills = new Bills({
+        document,
+        onNavigate,
+        store: null,
+        localStorage: window.localStorage,
+      });
+
+      const newBillButton = screen.getByTestId("btn-new-bill");
+      const handleClickNewBill = jest.fn(bills.handleClickNewBill);
+      newBillButton.addEventListener("click", handleClickNewBill);
+      fireEvent.click(newBillButton);
+
+      expect(handleClickNewBill).toHaveBeenCalled();
+      expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH["NewBill"]);
+    })
+  })
+})
+
